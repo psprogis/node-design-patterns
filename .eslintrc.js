ba3ee73b4cdd79813f7da057d1b78176074e5539
pavlo <pavlo.sprogis@harmonicinc.com>
2018-09-16 23:42:56 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    'extends': ['airbnb-base'],
+
+    globals: {
+    },
+
+    env: {
+        es6: true,
+        node: true,
+    },
+
+    parserOptions: {
+        'ecmaVersion': 2017,
+    },
+
+    rules: {
+        'indent': ['error', 4],
+        'max-len': ['error', { 'code': 120 }],
+        'padded-blocks': 'off',
+        'class-methods-use-this': 'off',
+        'arrow-parens': 'off',
+        'no-plusplus': 'off',
+        'no-prototype-builtins': 'warn',
+    },
+};
