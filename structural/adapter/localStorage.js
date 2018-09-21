66d9b520ea2d4c9b7185a9ae4dcb0d944e2e6bcd
pavel sprogis <pavel.sprogis@gmail.com>
2018-09-21 15:28:30 +0300

    add adapter example

---
+++ structural/adapter/localStorage.js
@@ -0,0 +1,47 @@
+/*
+ * Motivation: convert the interface of a class into another clients expect.
+ * Adapter lets classes work together that couldn't otherwise because of incompatible interfaces
+ */
+
+const { writeFile, existsSync, readFileSync, unlink } = require('fs');
+
+class LocalStorage {
+
+    constructor() {
+        if (existsSync('localStorage.json')) {
+            console.log('Loading items from localStorage.json');
+
+            const txt = readFileSync('localStorage.json');
+            this.items = JSON.parse(txt);
+        } else {
+            this.items = {};
+        }
+    }
+
+    get length() {
+        return Object.keys(this.items).length;
+    }
+
+    getItem(key) {
+        return this.items[key];
+    }
+
+    setItem(key, value) {
+        this.items[key] = value;
+        writeFile('localStorage.json', JSON.stringify(this.items), error => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    }
+
+    clear() {
+        this.items = {};
+        unlink('localStorage.json', () => {
+            console.log('localStorage file removed');
+        });
+    }
+
+}
+
+module.exports = new LocalStorage();
