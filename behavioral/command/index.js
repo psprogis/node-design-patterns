f7d6d9a0df00ead0fc52e180b55a08ab8bb21089
pavel sprogis <pavel.sprogis@gmail.com>
2018-09-25 14:13:59 +0300

    add command pattern example

---
+++ behavioral/command/index.js
@@ -0,0 +1,50 @@
+/*
+ * Motivation: encapsulate a request as an object, thereby letting you parameterize with
+ * different requests, queue or log requests, and support undoable operations.
+ */
+
+const { createInterface } = require('readline');
+const conductor = require('./conductor');
+const { ExitCommand, CreateCommand } = require('./commands');
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log('create <fileName> <text> | history | undo | redo | exit');
+rl.prompt();
+
+rl.on('line', input => {
+
+    const [commandText, ...remaining] = input.split(' ');
+    const [fileName, ...fileText] = remaining;
+    const text = fileText.join(' ');
+
+    switch (commandText) {
+    case 'history':
+        conductor.printHistory();
+        break;
+
+    case 'undo':
+        conductor.undo();
+        break;
+
+    case 'redo':
+        conductor.redo();
+        break;
+
+    case 'exit':
+        conductor.run(new ExitCommand());
+        break;
+
+    case 'create':
+        conductor.run(new CreateCommand(fileName, text));
+        break;
+
+    default:
+        console.log(`${commandText} command not found!`);
+    }
+
+    rl.prompt();
+});
