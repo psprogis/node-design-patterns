1860a719c67d7ab66d06868f691c92309c9f6595
pavel sprogis <pavel.sprogis@gmail.com>
2018-09-26 21:08:57 +0300

    add iterator example

---
+++ behavioral/iterator/Iterator.js
@@ -0,0 +1,43 @@
+class Iterator {
+
+    constructor(items = []) {
+        this.index = 0;
+        this.items = items;
+    }
+
+    first() {
+        const [first] = this.items;
+        return first;
+    }
+
+    last() {
+        // const [last] = [...this.items].reverse();
+
+        // do not return reference, use clone instead ?
+        return this.items[this.items.length - 1];
+    }
+
+    hasNext() {
+        return this.index < this.items.length - 1;
+    }
+
+    current() {
+        return this.items[this.index];
+    }
+
+    next() {
+        if (this.hasNext()) {
+            this.index += 1;
+        }
+        return this.current();
+    }
+
+    prev() {
+        if (this.index !== 0) {
+            this.index -= 1;
+        }
+        return this.current();
+    }
+}
+
+module.exports = Iterator;
